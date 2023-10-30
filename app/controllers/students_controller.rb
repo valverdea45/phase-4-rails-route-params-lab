65ce8ff3c958a8a2c16ab88696c1abf8c84d6675
Adrian <adrianvalverde45@gmail.com>
2023-10-30 14:13:43 -0400

    commit tree f22013a4ac9c0fb272404b1e6fabfba88e0853da

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
     render json: students
   end
+
+  def show
+
+    student = Student.find(params[:id])
+
+    render json: student
+
+  end
+
 
 end
